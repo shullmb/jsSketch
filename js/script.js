229e6d97f12e9553457aefb8cb3a9b909b0b1839
shullmb <dev.shullmb@gmail.com>
2015-08-12 15:27:49 -0700

    incomplete

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 $(document).ready( function(){
-	$('.sqr').hover( function(){
-		$(this).css('background','#0f0f0f');
+	$('.sqr').click( function(){
+		$(this).toggleClass('sketched');
 	});
+//	$('.sqr').click( function(){
+//		$(this).removeClass('sketched');
+//	});
 });
 //$(document){
 //};
