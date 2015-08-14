3bc65f73d134723a42bf8e53cd2d47a78fbc15c4
shullmb <dev.shullmb@gmail.com>
2015-08-13 21:14:25 -0700

    functional - need dynamic grid

--- js/script.js
+++ js/script.js
@@ -3,10 +3,9 @@ $(document).ready( function(){
 	$('#header button[name="grid"]').click( function(){
 		var numDivs= prompt("Please enter a grid between 4 and 64")
 		var grd = '<div class="sqr"></div>';
-		for (i=1; i <= numDivs; i++){
+		for (i=0; i < numDivs; i++){
 			$('#container').append(grd);
 		}	
-		//$('#container').append('<div class="sqr"></div>');
 	});
 //Reset button to erase 'sketch'
 	$('#header button[name="reset"]').click( function(){
@@ -14,7 +13,10 @@ $(document).ready( function(){
 	});
 //Sketch!
 	$(document).on('mouseenter', '.sqr', function() {
-			console.log("hovering");
-			$(this).addClass('sketched'); 
+			$(this).addClass('sketched');
+			$(this).addClass('highlight');
+	});
+	$(document).on('mouseleave','.sqr', function(){
+			$(this).removeClass('highlight');
 	});
 });
